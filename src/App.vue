fd91e190a886e0140547fff9c5fc2aee4b8b6974
Marlene <ruthboko23@gmail.com>
2024-08-21 02:21:53 +0100

    App overflow

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative flex flex-col lg:ml-[120px] lg:mr-[120px] p-2 gap-20 md:ml-[46px] md:mr-[46px] ml-[20px] ">
+  <div class="relative flex flex-col lg:ml-[120px] lg:mr-[120px] p-2 gap-20 md:ml-[46px] md:mr-[46px] ml-[20px] overflow-hidden ">
     <div class="absolute inset-0 z-0">
       <div class="absolute top-0 w-px h-full bg-[#E8E9EA] left-2"></div>
       <div class="absolute ml-[-200px] md:w-[955px] sm:w-[375px] lg:w-[1599px] w-[800px] h-px bg-[#E8E9EA] top-24 "></div>
